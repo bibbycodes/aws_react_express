2d586042aee14873f7132bb5911f247054a53aa4
Robert <bibbycodes@gmail.com>
2020-02-02 16:27:23 +0000

    serving static files from express

---
+++ client/build/precache-manifest.aad63a27e1cee2d7ff84430214f53663.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7c2b55195aa4d070ac60aeed383e4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60719eb71e4ad0f6590d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "aaf08964b565633666d7",
+    "url": "/static/js/2.76edaa05.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.76edaa05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60719eb71e4ad0f6590d",
+    "url": "/static/js/main.6ebcba12.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);

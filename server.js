3bf8014fa9e0adc244bfb534a54480b22e37ce31
Robert <bibbycodes@gmail.com>
2020-02-02 16:27:53 +0000

    build and static

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express =  require('express');
 const PORT = process.env.HTTP_PORT || 4001;
 const app = express();
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.get('/', (req, res) => {
   res.send('This is an AWS tut');
